947ba8a0a37c154ce98dd2e0e2b1c93562400570
JSebastianCK <jsebastianck@gmail.com>
2018-11-22 22:20:03 -0300

    Ejercicio5: The Beginning

---
+++ TPON-3/ejercicio5/Visitante.java
@@ -0,0 +1,14 @@
+package ejercicio5;
+
+public class Visitante implements Runnable{
+	private String nombre;
+	
+	public Visitante(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void run() {
+		
+	}
+	
+}
